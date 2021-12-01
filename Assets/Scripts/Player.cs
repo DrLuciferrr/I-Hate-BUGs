b19de05c5d0893781d3de7b5939ac3c77640a873
DrLuciferrr <drlucifer17@gmail.com>
2021-12-01 21:22:37 +0200

    ПОЧТИ ГОТОВО

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,32 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public float baseStressLevel;
+    [SerializeField] private GameObject stressMether;
+    [Space]
+    public float baseStress;
+    private float stress = 0f;
 
-    [SerializeField] private float stress = 0f;
-    [SerializeField] private Slider stressMether;
-    [SerializeField] private Text stressValue;
 
+    private float stressToScale = 2.1f;
     private void Awake()
     {
-        StressChange(baseStressLevel);
+        StressChange(baseStress);
     }
 
     //Метод изменения стресса(как вверх так и вниз) + отображение результата в StressMether
     public void StressChange(float stressFactor) 
     {
         stress += stressFactor;
+
         if (stress < 0)
             stress = 0;
 
-        stressMether.value = stress;
-        stressValue.text = stressMether.value.ToString();
+        stressMether.transform.localScale = new Vector3 (4, stress * stressToScale, 1);
 
         //Условие поражения
         if (stress >= 100)
             GameOver();
     }
-
 
 
     //Метод, вызываемый при успловиях поражения
